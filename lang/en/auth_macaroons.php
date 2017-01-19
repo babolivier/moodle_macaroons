e7a919c46cd93ac0baecfbd881a84e2046b13179
Brendan Abolivier <contact@brendanabolivier.com>
2017-01-19 15:31:00 +0000

    Switching strings to locale instead of having them hardcoded

--- lang/en/auth_macaroons.php
+++ lang/en/auth_macaroons.php
@@ -24,3 +24,17 @@
 
 $string['auth_macaroonsdescription'] = 'Macaroons usage for authentication';
 $string['pluginname'] = 'Macaroons';
+$string['cookie_name_label'] = 'Cookie name';
+$string['cookie_name_help'] = 'Name of the cookie your macaroon is located in.';
+$string['secret_label'] = 'Secret';
+$string['secret_help'] = 'The secret your macaroon was signed with.';
+$string['identifier_format_label'] = 'Identifier format';
+$string['identifier_format_help'] = 'Your Macaroon\'s identifier format. Available placeholders are {{username}}, {{firstname}}, {{lastname}}. Elements must me delimited with semicolons (";").<br />eg: {{firstname}};{{lastname}}';
+$string['email_config_label'] = 'E-mail template';
+$string['email_config_help'] = 'Template for emails. Available placeholders are {{firstname}} and {{lastname}}.<br />eg: {{firstname}}.{{lastname}}@company.tld';
+$string['caveat1_condition_label'] = 'First caveat condition (optional)';
+$string['caveat1_condition_help'] = 'The condition in your macaroon\'s first caveat.';
+$string['caveat2_condition_label'] = 'Second caveat condition (optional)';
+$string['caveat2_condition_help'] = 'The condition in your macaroon\'s second caveat';
+$string['caveat3_condition_label'] = 'Third caveat condition (optional)';
+$string['caveat3_condition_help'] = 'The condition in your macaroon\'s third caveat';
